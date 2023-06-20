9e2f91eb35bce84f5e0522bd5fae0c4a66f9bce8
Daniela Liendo <daniela@ualabee.com>
2023-06-20 11:34:21 +0200

    more page component was created

---
+++ src/app/twitter/pages/more-page/more-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-more-page',
+  templateUrl: './more-page.component.html',
+  styleUrls: ['./more-page.component.css']
+})
+
+export class MorePageComponent {}
