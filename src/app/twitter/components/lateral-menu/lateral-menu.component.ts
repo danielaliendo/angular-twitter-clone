3d2557aff2eadd42d58eaab5218bd554fcab8322
Daniela Liendo <daniela@ualabee.com>
2023-06-20 11:39:18 +0200

    lateral menu component was created

---
+++ src/app/twitter/components/lateral-menu/lateral-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-lateral-menu',
+  templateUrl: './lateral-menu.component.html',
+  styleUrls: ['./lateral-menu.component.css']
+})
+
+export class LateralMenuComponent {}
