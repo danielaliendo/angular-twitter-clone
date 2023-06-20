9bb7ea9f4594b0a69dd1eb4366e963012d2e168e
Daniela Liendo <daniela@ualabee.com>
2023-06-20 11:38:24 +0200

    explore page component was created

---
+++ src/app/twitter/pages/explore-page/explore-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-explore-page',
+  templateUrl: './explore-page.component.html',
+  styleUrls: ['./explore-page.component.css']
+})
+
+export class ExplorePageComponent {}
