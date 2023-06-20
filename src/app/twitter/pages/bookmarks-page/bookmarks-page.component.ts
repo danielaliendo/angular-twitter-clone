bd9d0f8a06037745d3e78ee139f860af82e62145
Daniela Liendo <daniela@ualabee.com>
2023-06-20 11:38:42 +0200

    bookmarks page component was created

---
+++ src/app/twitter/pages/bookmarks-page/bookmarks-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-bookmarks-page',
+  templateUrl: './bookmarks-page.component.html',
+  styleUrls: ['./bookmarks-page.component.css']
+})
+
+export class BookmarksPageComponent {}
