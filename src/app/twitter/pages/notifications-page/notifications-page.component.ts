74508ec942b92a3b4e685fdfe93933ae2dfcd234
Daniela Liendo <daniela@ualabee.com>
2023-06-20 11:34:07 +0200

    notifications page component was created

---
+++ src/app/twitter/pages/notifications-page/notifications-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-notifications-page',
+  templateUrl: './notifications-page.component.html',
+  styleUrls: ['./notifications-page.component.css']
+})
+
+export class NotificationsPageComponent {}
