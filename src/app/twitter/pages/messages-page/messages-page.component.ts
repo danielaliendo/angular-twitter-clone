5a0af02e885a48231427a0cf6aab505c45fbecb5
Daniela Liendo <daniela@ualabee.com>
2023-06-20 11:34:39 +0200

    messages page component was created

---
+++ src/app/twitter/pages/messages-page/messages-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-messages-page',
+  templateUrl: './messages-page.component.html',
+  styleUrls: ['./messages-page.component.css']
+})
+
+export class MessagesPageComponent {}
